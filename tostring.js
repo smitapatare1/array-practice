232a92ee189e2a6f8dfa4ef97ac15ab9f61a8232
smitapatare1 <smitapatare2002>
2022-10-29 21:13:18 +0530

    tostring method is here

---
+++ tostring.js
@@ -0,0 +1,8 @@
+const arr=["satyam","priyanka","rutuja","mansi","vaishanavi"]
+console.log(arr)
+console.log(arr.toString())      //this method converts the array into atring i.e Array -> String  eg. ['A','B'].tostring()  o/p "A,B"
+
+//output
+//node tostring.js
+//[ 'satyam', 'priyanka', 'rutuja', 'mansi', 'vaishanavi' ]...................in array format
+//satyam,priyanka,rutuja,mansi,vaishanavi.........................................convert to string
