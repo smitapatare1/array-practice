83c9664bdc27f9e6266bb542d390f88b24624e0a
smitapatare1 <smitapatare2002>
2022-10-29 20:54:15 +0530

    push.js added

---
+++ push.js
@@ -0,0 +1,16 @@
+const arr = [ ]
+arr.push("smita")
+arr.push("pooja")
+console.log(arr)
+arr.push("sakshi")
+arr.push("pratiksha")
+console.log(arr)
+arr.push("rutika")
+arr.push("priya")
+console.log(arr)
+
+//output   ==>>  
+//PS C:\Users\SAI\Desktop\array-practice> node push.js  
+[// 'smita', 'pooja' ]
+//[ 'smita', 'pooja', 'sakshi', 'pratiksha' ]
+[// 'smita', 'pooja', 'sakshi', 'pratiksha', 'rutika', 'priya' ] 
