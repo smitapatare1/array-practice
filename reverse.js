94499941a40d6e28cb0ebc6be4be4bb8844b7300
smitapatare1 <smitapatare2002>
2022-10-29 20:46:56 +0530

    reverse.js  code added

---
+++ reverse.js
@@ -0,0 +1,12 @@
+const arr = ["satyam","dhirendra","vaibhav","avi"]
+console.log(arr)
+arr.reverse()                     //if we want to reverse the array then revers() method is used..
+console.log(arr)
+
+
+
+
+//o/p
+//node reverse.js
+//[ 'satyam', 'dhirendra', 'vaibhav', 'avi' ]
+//[ 'avi', 'vaibhav', 'dhirendra', 'satyam' ]
