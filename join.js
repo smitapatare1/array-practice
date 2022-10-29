7274daf06be317d30571563bf390bcf20a9277a1
smitapatare1 <smitapatare2002>
2022-10-29 21:20:46 +0530

    join method

---
+++ join.js
@@ -0,0 +1,10 @@
+const arr = ["satyam","mayur","anmol","pawan","akshay"]
+console.log(arr)
+console.log(arr.join("#"))      //join() method is similar to toString() method but we can specify seprator(space, doller $, hash #)
+
+
+
+//output
+// node join.js
+//[ 'satyam', 'mayur', 'anmol', 'pawan', 'akshay' ]
+//satyam#mayur#anmol#pawan#akshay    ............................here hash# is the seprator
