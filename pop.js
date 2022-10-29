63bcca7d7e59052691478a9790000aae8195de8d
smitapatare1 <smitapatare2002>
2022-10-29 20:59:38 +0530

    pop.js methoad added....

---
+++ pop.js
@@ -0,0 +1,7 @@
+const arr = ["satyam","purva","vishwaja","aditi","suvarna"]
+arr.pop()                           //pop() method used to removes last element from an array
+console.log(arr)
+
+//o/p
+//PS C:\Users\SAI\Desktop\array-practice> node pop.js   
+//[ 'satyam', 'purva', 'vishwaja', 'aditi' ]
