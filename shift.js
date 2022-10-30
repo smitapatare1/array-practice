af231551a84a4b37d145d58ec294858939935a67
smitapatare1 <smitapatare2002>
2022-10-30 17:50:53 +0530

    shift method is completed

---
+++ shift.js
@@ -0,0 +1,10 @@
+//shift() method is used to remove strarting (1st) element...
+//eg.
+const arr=["shreyas","vaibhav","pranav","kishor","yogesh","rahul"]
+console.log(arr)
+arr.shift()
+console.log(arr)
+
+//output
+//[ 'shreyas', 'vaibhav', 'pranav', 'kishor', 'yogesh', 'rahul' ]
+//[ 'vaibhav', 'pranav', 'kishor', 'yogesh', 'rahul' ]
